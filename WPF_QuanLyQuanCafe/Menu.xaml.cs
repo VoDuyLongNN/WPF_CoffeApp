9f6b77e05a4a5e3cadd307f47e6b9ed4673e4d2b
Võ Duy Long <89375882+VoDuyLongNN@users.noreply.github.com>
2022-04-15 00:36:11 +0700

    upload

---
+++ WPF_QuanLyQuanCafe/Menu.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_QuanLyQuanCafe
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public int getTbxQuantily()
+        {
+            return int.Parse(txbQuantity.Text);
+        }
+
+        public void setTxbQuantily(int n)
+        {
+            txbQuantity.Text = n.ToString();
+        }
+
+        private void lbItemProduct_Selected(object sender, RoutedEventArgs e)
+        {
+            ProductDescription formDesc = new ProductDescription();
+            formDesc.imgDesc.Source = imgProduct.Source;
+            formDesc.NameDesc.Text = nameProduct.Text;
+            formDesc.priceDesc.Text = price.Text;
+            formDesc.ShowDialog();
+        }
+    }
+}
